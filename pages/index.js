44c2522e71b104f2552b52e2261853c158d56f1f
Pulkit Bhutani <pulkitbhutani@outlook.com>
2021-04-15 10:03:39 +0530

    checkin with new env file and votes and homepage places.

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,14 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
     return (
         <main className={styles.main}>
-            <h1 className={styles.title}>Welcome to HackerNews</h1>
+            <h1 className={styles.title}>Trending News</h1>
+
+
+            <h1 className={styles.title}>Asks</h1>
+
+
+            <h1 className={styles.title}>Recent News</h1>
+
         </main>
     );
 }
