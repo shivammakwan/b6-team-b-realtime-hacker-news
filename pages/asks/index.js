9bb256fad964f050046a59da8f90c7a68b6735ea
Pulkit Bhutani <pulkitbhutani@outlook.com>
2021-04-17 00:36:31 +0530

    Add News and Add Asks forms and post api's.

--- pages/asks/index.js
+++ pages/asks/index.js
@@ -1,6 +1,19 @@
+import Link from "next/link";
+import styles from "../../styles/Home.module.css";
+import Button from "../../components/Button";
+
 export default function Asks() {
     return (
-        <h1>Asks Page</h1>
+        <div className="grid grid-rows-1 grid-flow-col">
+        <div className="col-span-8 ">
+            <h1 className={styles.title}>Asks Page</h1>
+        </div>
+        <div>
+            <Link href="/asks/addAsk" passHref>
+                <Button text={"Ask HN"} />
+            </Link>
+        </div>
+    </div>
     );
 
 }
