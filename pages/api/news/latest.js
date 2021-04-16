8459a5513062353e6537e521c98c4cabb75dc484
Deepak Priyadarshi <myselfdeepak9405@gmail.com>
2021-04-16 17:31:59 +0530

    Created Latest News API

---
+++ pages/api/news/latest.js
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+export default async function (request, response) {
+    const prisma = new PrismaClient({ log: ["query"] });
+
+    try {
+        const latestAsks = await prisma.post_master.findMany({
+            orderBy: [
+                {
+                    createdAt: "desc",
+                },
+            ],
+        });
+
+        response.status(200).json(latestAsks);
+    } catch (error) {
+        response.status(500).json({
+            status: "error",
+            message: "Unable To Fetch Latest Ask Posts",
+        });
+    }
+}
