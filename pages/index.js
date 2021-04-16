7e1f546dbde4a3e6d790f2f9c89a1bed89e43384
Deepak Priyadarshi <myselfdeepak9405@gmail.com>
2021-04-16 19:16:52 +0530

    Added News Post Section On Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,46 @@
+import NewsPost from "../components/NewsPost";
 import styles from "../styles/Home.module.css";
 
+import useSWR from "swr";
+
+const fetchLatestNewsPost = (url) => fetch(url).then((r) => r.json());
+
 export default function Home() {
+    const { data: latestNewsPost, error: latestNewsPostError } = useSWR("/api/news/latest", fetchLatestNewsPost("/api/news/latest"));
+
     return (
-        <main className={styles.main}>
-            <h1 className={styles.title}>Trending News</h1>
+        <>
+            <div className={styles.main}>
+                <h1 className={styles.title}>Trending News</h1>
 
+                <h1 className={styles.title}>Asks</h1>
 
-            <h1 className={styles.title}>Asks</h1>
+                <h1 className={styles.title}>Recent News</h1>
+            </div>
 
-
-            <h1 className={styles.title}>Recent News</h1>
-
-        </main>
+            <section className="home-news-post pl-10 pr-10">
+                <h1 className="text-3xl font-bold home-news-post-title">News Posts</h1>
+                {latestNewsPostError && (
+                    <h1 className="text-center text-xl text-red-300 py-10">Failed To Get Latest News Post, Try Again.</h1>
+                )}
+                {!latestNewsPost && <h1 className="text-center text-xl text-gray-500 py-10">Loading News Post....</h1>}
+                {latestNewsPost && (
+                    <div className="mt-5 flex flex-wrap justify-between">
+                        {latestNewsPost.slice(0, 8).map((newsPost, index) => {
+                            return (
+                                <NewsPost
+                                    key={newsPost.id}
+                                    title={newsPost.title}
+                                    description={newsPost.description}
+                                    postDate={newsPost.createdAt}
+                                    postImage={newsPost.imageUrl}
+                                    noOfPoints={newsPost.votes}
+                                />
+                            );
+                        })}
+                    </div>
+                )}
+            </section>
+        </>
     );
 }
