377e422d515661da508f9814ae2b20b373902e6f
Pulkit Bhutani <pulkitbhutani@outlook.com>
2021-04-18 21:52:44 +0530

    fixed, local build issue.
    Constant defiened by javed had different name and hence the one I created started failing after the merge, hence changed and tested on local, deploying directly to develop branch.

--- components/TrendingNews/TrendingItem/index.js
+++ components/TrendingNews/TrendingItem/index.js
@@ -9,7 +9,7 @@ const TrendingItem = ({ onClick, imageUrl, date, title,id }) => {
           <img src={imageUrl} className="rounded-lg h-24 w-28 hover:opacity-70 " />
         <div className="flex flex-col ml-2 mt-1 pl-1 justify-start overflow-hidden">
         <div className="flex flex-row items-center mt-2">
-        <svg class="h-6" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-lg font-serif text-gray-600 hover:text-green-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg className="h-6" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-lg font-serif text-gray-600 hover:text-green-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
         </svg>
         <span className="ml-1 items-center text-sm font-serif text-gray-500 hover:text-green-300">{date}</span>
