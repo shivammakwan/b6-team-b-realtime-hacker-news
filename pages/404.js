81c3134d272589ec49bd915dc248d8167ed742cd
abduljavedkhan <abduljaved314@gmail.com>
2021-04-20 16:04:08 +0530

    Bug fix

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function () {
+    return (
+        <div>
+            <h1 className="text-center text-base md:text-3xl text-red-400 py-8 px-10">Resource Not found</h1>
+        </div>
+    );
+}
