f6fb44752d7df347dde34e0a217e12199b447d79
Pulkit Bhutani <pulkitbhutani@outlook.com>
2021-05-06 19:33:24 +0530

    added ask posts to home page.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import styles from "../styles/Home.module.css";
 import TrendingNews from "../components/TrendingNews";
 import HomeNewsSection from "../components/HomeNewsSection";
+import HomeAskSection from "../components/HomeAskSection";
 
 export default function Home() {
     return (
         <>
             <TrendingNews />
+            <HomeAskSection/>
             <HomeNewsSection />
         </>
     );
