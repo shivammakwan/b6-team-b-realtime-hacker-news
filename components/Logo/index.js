0459a49dbf0de76ecd88df106f3252f5ca4f4c91
Pulkit Bhutani <pulkitbhutani@outlook.com>
2021-05-06 16:54:13 +0530

    fix login page responsiveness.

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
     return (
         <div className="justify-center items-center text-center text-primary">
-            <img className=" mb-2 object-contain" src="assets/images/logo-light.png" alt="logo" />
+            <img className="mb-2 object-contain" src="assets/images/logo-light.png" alt="logo" />
         </div>
     );
 }
