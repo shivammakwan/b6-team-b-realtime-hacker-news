55fb84eeb79b4d74177325c7c8b31a69543e5f3e
Pulkit Bhutani <pulkitbhutani@outlook.com>
2021-05-06 16:19:06 +0530

    fixes for forms and comment fix.

--- components/TextInput/index.js
+++ components/TextInput/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const TextInput = ({ text, setText, placeholder = '', isAutoFocus = true, required = false, upperCase = false, requiredMsg = "Required", check = false }) => {
     return (
-        <div className="my-2 mx-2">
-            <input className="bg-gray-100 rounded-md w-96 h-10 mb-1 text-left pl-4 text-2xl shadow-lg lg:text-sm"
+        <div className="flex flex-col mb-4">
+            <input className="border border-gray-400 py-2 px-3 text-grey-darkest" 
                 type="text"
                 placeholder={placeholder}
                 value={text}
--- utility/constants.js
+++ utility/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:3000/api";
+export const URL = "https://realtime-hacker-news-git-develop-shivammakwan.vercel.app/api";
 
 export const POST_TYPE_NEWS = "news";
 export const POST_TYPE_ASK = "ask";
