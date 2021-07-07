4ec33b7fb4147425f6f32b2c377860c001aa7d23
abduljavedkhan <abduljaved314@gmail.com>
2021-07-07 17:55:53 +0530

    minor fix

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <>
             <TrendingNews />
-            <HomeAskSection/>
+            {/* <HomeAskSection/> */}
             <HomeNewsSection />
         </>
     );
