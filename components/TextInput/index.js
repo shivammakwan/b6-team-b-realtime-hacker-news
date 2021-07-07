d684a8fbbadda59fa645517cc955c2807e7e2ca1
abduljavedkhan <abduljaved314@gmail.com>
2021-07-07 17:51:25 +0530

    bug fix

--- components/TextInput/index.js
+++ components/TextInput/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TextInput = ({ text, setText, placeholder = '', isAutoFocus = true, required = false, upperCase = false, requiredMsg = "Required", check = false }) => {
     return (
         <div className="flex flex-col mb-4">
-            <input className="border border-gray-400 py-2 px-3 text-grey-darkest" 
+            <input className="border border-gray-400 py-2 px-3 text-grey-darkest rounded-lg" 
                 type="text"
                 placeholder={placeholder}
                 value={text}
